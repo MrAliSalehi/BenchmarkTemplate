d1cc535243ce02ae425a64334500b825af30dac9
Qwxp <mmdjoker@yahoo.com>
2022-07-14 21:54:16 +0430

    Add project files.

---
+++ Benchmarks/MyAwesomeBenchmark.cs
@@ -0,0 +1,21 @@
+﻿
+using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkTemplate.Benchmarks;
+
+// pass false if you don't want to see Gen column's
+[MemoryDiagnoser]
+public class MyAwesomeBenchmark
+{
+    [Benchmark]
+    public void MyMethod()
+    {
+
+    }
+
+    [Benchmark]
+    public bool MyCuteMethod()
+    {
+        return true;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using BenchmarkTemplate.Benchmarks;
+
+namespace BenchmarkTemplate;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<MyAwesomeBenchmark>();
+
+        Console.ReadKey();
+    }
+}
